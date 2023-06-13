18ef30704b78eaec85620d8ae21f42ee2307ed9f
ashishamruthaluri <119551943+ashishamruthaluri@users.noreply.github.com>
2023-06-13 11:42:30 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CLOCK";
+const accessCode2 = "FIREBASE";
+const accessCode3 = "GLOBALAREA";
+
+function clues() {
+    
+    fill("aqua")
+    textSize(15)
+    text("K O C L C", 100,50)
+    fill("lightblue")
+    text("Hint: Has three hands !!", 100,70)
+
+    fill("aqua")
+    textSize(15)
+    text("S A B F I R E E", 700,150)
+    fill("lightblue")
+    text("Hint: Data/ref !!", 700,170)
+
+    fill("aqua")
+    textSize(15)
+    text("A G O L B A R L A E", 100,250)
+    fill("lightblue")
+    text("Hint: top part of code  !!", 100,270)
+
+}
